b5c9e16a8cd22922c944d83c8ef42b82bfb54874
Reinhardt Botha <botha.wr@gmail.com>
2024-05-23 10:11:44 -0700

    feat: add example code for Expo BackgroundFetch

--- App.tsx
+++ App.tsx
@@ -1,10 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import * as BackgroundFetch from "expo-background-fetch";
+import * as TaskManager from "expo-task-manager";
+
+import BackgroundFetchScreen from "./components/BackgroundFetchScreen";
+import { useState, useEffect } from "react";
+
+const BACKGROUND_FETCH_TASK = "task1";
+
+let counter = 0;
+
+// 1. Define the task by providing a name and the function that should be executed
+// Note: This needs to be called in the global scope (e.g outside of your React components)
+TaskManager.defineTask(BACKGROUND_FETCH_TASK, async () => {
+  console.log(`Background fetch called`);
+  counter += 1;
+
+  // Be sure to return the successful result type!
+  return BackgroundFetch.BackgroundFetchResult.NewData;
+});
 
 export default function App() {
+  const [fetchCounter, setFetchCounter] = useState(counter);
+
+  // Update the fetchCounter state every second to reflect the latest counter value
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setFetchCounter(counter);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <BackgroundFetchScreen
+        backgroundFetchTask={BACKGROUND_FETCH_TASK}
+        counter={fetchCounter}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +46,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
