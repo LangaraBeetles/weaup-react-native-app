75f14db48e67bc49bed5afc7ae5e4944589e32cc
zeckkimon <bsaguid@gmail.com>
2024-06-06 05:38:59 -0700

    feat: implement foreground tracking with haptic feedback

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -4,7 +4,7 @@ import { useUser } from "@state/useUser";
 import { Box } from "@gluestack-ui/themed";
 import { Redirect } from "expo-router";
 import { useBackgroundTasks } from "@src/components/providers/BackgroundTasksProvider";
-
+import DeviceMotionView from "@src/components/ui/DeviceMotionView";
 const styles = StyleSheet.create({
   text: {
     ...globalStyles,
@@ -62,11 +62,12 @@ const HomePage = () => {
       <Button
         title={
           isTrackingEnabled
-            ? "Disable Background Fetch"
-            : "Enable Background Fetch"
+            ? "Disable Tracking"
+            : "Enable Tracking"
         }
         onPress={toggleBackgroundFetch}
       />
+      <DeviceMotionView/>
     </Box>
   );
 };
