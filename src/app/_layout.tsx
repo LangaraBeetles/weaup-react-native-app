ec637bd356590693d6be1d6d6726287f4077e38e
Wonnyo Hamester <57273227+whamester@users.noreply.github.com>
2024-06-06 20:40:35 -0700

    feat: TrackingModeProvider (#13)

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,19 +1,22 @@
 import { GluestackUIProvider } from "@gluestack-ui/themed";
 import BackgroundTasksProvider from "@src/components/providers/BackgroundTasksProvider";
 import HeadTrackingProvider from "@src/components/providers/HeadTrackingProvider.ios";
+import TrackingModeProvider from "@src/components/providers/TrackingModeProvider";
 import { Stack } from "expo-router/stack";
 
 const RootLayout = () => {
   return (
     <BackgroundTasksProvider>
-    <HeadTrackingProvider>
-      <GluestackUIProvider>
-        <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="setup" options={{ headerShown: false }} />
-        </Stack>
-      </GluestackUIProvider>
-    </HeadTrackingProvider>
+      <TrackingModeProvider>
+        <HeadTrackingProvider>
+          <GluestackUIProvider>
+            <Stack>
+              <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+              <Stack.Screen name="setup" options={{ headerShown: false }} />
+            </Stack>
+          </GluestackUIProvider>
+        </HeadTrackingProvider>
+      </TrackingModeProvider>
     </BackgroundTasksProvider>
   );
 };
---
+++ src/components/providers/TrackingModeProvider.tsx
@@ -0,0 +1,59 @@
+import { TrackingModeType } from "@src/interfaces/user.types";
+import { useUser } from "@src/state/useUser";
+import { createContext, useEffect } from "react";
+import {
+  onHeadphoneConnected,
+  onHeadphoneDisconnected,
+  isHeadphoneMotionAvailable,
+  startDeviceMotionUpdates,
+} from "react-native-headphone-motion";
+
+type ContextState = {
+  mode: TrackingModeType;
+};
+
+const initialState: ContextState = {
+  mode: "PHONE",
+};
+
+const TrackingModeContext = createContext<ContextState>(initialState);
+
+const TrackingModeProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const mode = useUser((state) => state.mode);
+  const changeMode = useUser((state) => state.changeMode);
+
+  useEffect(() => {
+    startDeviceMotionUpdates();
+
+    const connectedListener = onHeadphoneConnected(() => {
+      console.log("earbuds connected");
+      if (isHeadphoneMotionAvailable) {
+        changeMode("EARBUDS");
+      }
+    });
+
+    const disconnectedListener = onHeadphoneDisconnected(() => {
+      console.log("earbuds disconnected");
+      changeMode("PHONE");
+    });
+
+    return () => {
+      connectedListener.remove();
+      disconnectedListener.remove();
+    };
+  }, []);
+
+  return (
+    <TrackingModeContext.Provider
+      value={{
+        mode,
+      }}
+    >
+      {children}
+    </TrackingModeContext.Provider>
+  );
+};
+
+export default TrackingModeProvider;
