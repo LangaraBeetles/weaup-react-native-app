fee2f0038ed4bc83ce87cc6f59b2cd2a42c4edd5
Reinhardt Botha <botha.wr@gmail.com>
2024-06-16 17:52:28 -0700

    feat: remove background fetch

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,19 +1,16 @@
 import { GluestackUIProvider } from "@gluestack-ui/themed";
-import BackgroundTasksProvider from "@src/components/providers/BackgroundTasksProvider";
 import PushNotificationsProvider from "@src/components/providers/PushNotificationsProvider";
 import { Stack } from "expo-router/stack";
 
 const RootLayout = () => {
   return (
     <PushNotificationsProvider>
-      <BackgroundTasksProvider>
-        <GluestackUIProvider>
-          <Stack>
-            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            <Stack.Screen name="setup" options={{ headerShown: false }} />
-          </Stack>
-        </GluestackUIProvider>
-      </BackgroundTasksProvider>
+      <GluestackUIProvider>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="setup" options={{ headerShown: false }} />
+        </Stack>
+      </GluestackUIProvider>
     </PushNotificationsProvider>
   );
 };
--- src/components/ui/DeviceMotionView.tsx
+++ src/components/ui/DeviceMotionView.tsx
@@ -1,73 +1,79 @@
-import { Text, View } from 'react-native';
-import React, { useState, useEffect } from 'react';
-import { DeviceMotion } from 'expo-sensors';
-import { useBackgroundTasks } from "@src/components/providers/BackgroundTasksProvider";
+import { Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import { DeviceMotion } from "expo-sensors";
 import * as Haptics from "expo-haptics";
 
-export default function DeviceMotionView() {
-    const [{beta, gamma }, setRotationData] = useState({beta: 0, gamma: 0});
-    const [orientation, setOrientation] = useState(0);
-    const [notification, setNotification] = useState(false);
-    const showNotification = () => setNotification(true);
-    const hideNotification = () => setNotification(false);
-    const { isTrackingEnabled } = useBackgroundTasks();
-    
-    //Get RotationData
-    useEffect(() => {
-        if(isTrackingEnabled){
-            _subscribe();
-        } else {
-            hideNotification();
-        }
-        return () => { _unsubscribe(); };
-    }, [isTrackingEnabled]);
+export default function DeviceMotionView(props: any) {
+  const [{ beta, gamma }, setRotationData] = useState({ beta: 0, gamma: 0 });
+  const [orientation, setOrientation] = useState(0);
+  const [notification, setNotification] = useState(false);
+  const showNotification = () => setNotification(true);
+  const hideNotification = () => setNotification(false);
+  const { isTrackingEnabled } = props;
 
-    //Get rotation and orientation values
-    const _subscribe = async () => {
-        try{
-            DeviceMotion.addListener((devicemotionData) => {
-                if(devicemotionData.rotation){
-                    setRotationData(devicemotionData.rotation);
-                }
-                setOrientation(devicemotionData.orientation);
-            });
-
-            DeviceMotion.setUpdateInterval(1000);
-        } catch(error){
-            console.error(error);
-        }
-    };
-
-    const _unsubscribe = () => {
-        DeviceMotion.removeAllListeners();
-    };
-
-    //Provide feedback
-    useEffect(()=>{
-        hideNotification();
-
-        if(beta < 0.03 && gamma < 0.03 ){ //device is on a flat surface  
-            return;
-        }
-
-        if(isBadPosture()) { 
-            Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy);
-            showNotification();
-        }
-    },[beta, gamma])
-
-    const isBadPosture = () => {
-        return  orientation == 0 && inBadPostureRange(beta) ||
-                orientation != 0 && inBadPostureRange(gamma)
+  //Get RotationData
+  useEffect(() => {
+    if (isTrackingEnabled) {
+      _subscribe();
+    } else {
+      hideNotification();
     }
-    
-    const inBadPostureRange = (data: number) => {
-        return data > 1.4 || data < 1
+    return () => {
+      _unsubscribe();
+    };
+  }, [isTrackingEnabled]);
+
+  //Get rotation and orientation values
+  const _subscribe = async () => {
+    try {
+      DeviceMotion.addListener((devicemotionData) => {
+        if (devicemotionData.rotation) {
+          setRotationData(devicemotionData.rotation);
+        }
+        setOrientation(devicemotionData.orientation);
+      });
+
+      DeviceMotion.setUpdateInterval(1000);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const _unsubscribe = () => {
+    DeviceMotion.removeAllListeners();
+  };
+
+  //Provide feedback
+  useEffect(() => {
+    hideNotification();
+
+    if (beta < 0.03 && gamma < 0.03) {
+      //device is on a flat surface
+      return;
     }
 
+    if (isBadPosture()) {
+      Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy);
+      showNotification();
+    }
+  }, [beta, gamma]);
+
+  const isBadPosture = () => {
     return (
-        <View>
-            <Text style={{ display: notification ? 'flex' : 'none' }}>Please check your posture.</Text>
-        </View>
+      (orientation == 0 && inBadPostureRange(beta)) ||
+      (orientation != 0 && inBadPostureRange(gamma))
     );
+  };
+
+  const inBadPostureRange = (data: number) => {
+    return data > 1.4 || data < 1;
+  };
+
+  return (
+    <View>
+      <Text style={{ display: notification ? "flex" : "none" }}>
+        Please check your posture.
+      </Text>
+    </View>
+  );
 }
