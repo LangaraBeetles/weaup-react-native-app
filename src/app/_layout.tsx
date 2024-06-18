26b66dc966e011f523a915c4a4bfffbd2dfa2a3e
Wonnnyo Hamester <wonnyohamester@outlook.com>
2024-06-18 11:34:06 -0700

    feat: fonts setup

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,13 +1,49 @@
 import PushNotificationsProvider from "@src/components/providers/PushNotificationsProvider";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
 import { Stack } from "expo-router/stack";
+import { useEffect } from "react";
 
 const RootLayout = () => {
+  const [fontsLoaded, fontError] = useFonts({
+    NunitoBlack: require("../../assets/fonts/NunitoBlack.ttf"),
+    NunitoExtraBold: require("../../assets/fonts/NunitoExtraBold.ttf"),
+    NunitoBold: require("../../assets/fonts/NunitoBold.ttf"),
+    NunitoSemiBold: require("../../assets/fonts/NunitoSemiBold.ttf"),
+    NunitoMedium: require("../../assets/fonts/NunitoMedium.ttf"),
+    NunitoRegular: require("../../assets/fonts/NunitoRegular.ttf"),
+    NunitoLight: require("../../assets/fonts/NunitoLight.ttf"),
+    NunitoExtraLight: require("../../assets/fonts/NunitoExtraLight.ttf"),
+    NunitoBlackItalic: require("../../assets/fonts/NunitoBlackItalic.ttf"),
+    NunitoExtraBoldItalic: require("../../assets/fonts/NunitoExtraBoldItalic.ttf"),
+    NunitoBoldItalic: require("../../assets/fonts/NunitoBoldItalic.ttf"),
+    NunitoSemiBoldItalic: require("../../assets/fonts/NunitoSemiBoldItalic.ttf"),
+    NunitoMediumItalic: require("../../assets/fonts/NunitoMediumItalic.ttf"),
+    NunitoItalic: require("../../assets/fonts/NunitoItalic.ttf"),
+    NunitoLightItalic: require("../../assets/fonts/NunitoLightItalic.ttf"),
+    NunitoExtraLightItalic: require("../../assets/fonts/NunitoExtraLightItalic.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontError) throw fontError;
+  }, [fontError]);
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <PushNotificationsProvider>
-        <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="setup" options={{ headerShown: false }} />
-        </Stack>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="setup" options={{ headerShown: false }} />
+      </Stack>
     </PushNotificationsProvider>
   );
 };
--- src/app/setup/start.tsx
+++ src/app/setup/start.tsx
@@ -1,8 +1,9 @@
 import Main from "@src/components/layout/Main";
 import Button from "@src/components/ui/Button";
 import Stack from "@src/components/ui/Stack";
+import { Text } from "@src/components/ui/typography";
 import { router } from "expo-router";
-import { SafeAreaView, Text, View } from "react-native";
+import { SafeAreaView, View } from "react-native";
 
 const SetupPages = () => {
   const next = () => {
@@ -22,7 +23,42 @@ const SetupPages = () => {
           <View style={{ height: "40%" }} />
           <Stack gap={80}>
             <Stack gap={16}>
-              <Text style={{ textAlign: "center" }}>Welcome to WeaUp</Text>
+              <Text align="center" level="title_1">
+                Welcome to WeaUp
+              </Text>
+              {/* <Text align="center" level="title_1">
+                Title 1
+              </Text>
+              <Text align="center" level="title_2">
+                Title 2
+              </Text>
+              <Text align="center" level="title_3">
+                Title 3
+              </Text>
+              <Text align="center" level="headline">
+                Headline
+              </Text>
+              <Text align="center" level="body">
+                Body
+              </Text>
+              <Text align="center" level="callout">
+                Callout
+              </Text>
+              <Text align="center" level="subhead">
+                Subhead
+              </Text>
+              <Text align="center" level="footnote">
+                Footnote
+              </Text>
+              <Text align="center" level="caption_1">
+                Caption 1
+              </Text>
+              <Text align="center" level="caption_2">
+                Caption 2
+              </Text>
+              <Text align="center" level="caption_3">
+                Caption 3
+              </Text> */}
               <Text style={{ textAlign: "center" }}>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                 eiusmod tempor.
