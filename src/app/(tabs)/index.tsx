cb131df4e0423ef1ef1c182a1f10c27868c83739
Reinhardt Botha <botha.wr@gmail.com>
2024-06-18 17:58:00 -0700

    chore: remove POC code from homepage

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -7,43 +7,14 @@ import DeviceMotionView from "@src/components/ui/DeviceMotionView";
 
 import { useUser } from "@state/useUser";
 
-// import { usePushNotifications } from "@src/components/providers/PushNotificationsProvider";
 import { globalStyles } from "@src/styles/globalStyles";
 import SessionControl from "@src/components/sessions/SessionControl";
 
 const HomePage = () => {
   const isSetupComplete = useUser((state) => state.isSetupComplete);
   const userName = useUser((state) => state.user.name);
-  // const setAuth = useUser((state) => state.setAuth);
 
   const [isTrackingEnabled, setTrackingEnabled] = useState(false);
-
-  // const { sendPushNotification } = usePushNotifications();
-
-  // const onNameChange = () => {
-  //   setAuth(true, {
-  //     deviceIds: ["1"],
-  //     currentDeviceId: "1",
-  //     dailyGoal: 80,
-  //     name: "Dr Seuss",
-  //   });
-  // };
-
-  // const onNameClear = () => {
-  //   setAuth(false, {
-  //     deviceIds: [],
-  //     currentDeviceId: null,
-  //     dailyGoal: 80,
-  //     name: "",
-  //   });
-  // };
-
-  // const handleSendNotification = async () => {
-  //   await sendPushNotification({
-  //     title: "Devs say:",
-  //     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-  //   });
-  // };
 
   if (!isSetupComplete) {
     return <Redirect href="/setup/start" />;
