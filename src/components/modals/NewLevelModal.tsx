458a24ac96f272ea5b7c39198ae58ec2b2dc9c15
Victorportus <vic.portus@gmail.com>
2024-06-20 14:03:02 -0700

    fix: Adjusted as requested

--- src/components/modals/NewLevelModal.tsx
+++ src/components/modals/NewLevelModal.tsx
@@ -1,6 +1,8 @@
 import React from "react";
-import { View, Text, Image, StyleSheet } from "react-native";
+import { View, Image, StyleSheet } from "react-native";
+import { Text } from "@src/components/ui/typography";
 import Modal from "react-native-modal";
+import { globalStyles } from "@src/styles/globalStyles";
 
 interface NewLevelModalProps {
   isVisible: boolean;
@@ -14,20 +16,20 @@ const NewLevelModal: React.FC<NewLevelModalProps> = ({
   return (
     <Modal isVisible={isVisible} onBackdropPress={onClose}>
       <View style={styles.modalContent}>
-        <Text style={styles.title}>You have reached Level 3</Text>
+        <Text style={styles.title} level="title_3">
+          You have reached Level 3
+        </Text>
         <View style={styles.levelContainer}>
-          <Text style={styles.levelLabel}>Level 2</Text>
-          <Text style={styles.levelLabel}>Level 3</Text>
+          <View style={styles.levelBarContainer} />
+          <Text level="caption_2">Level 2</Text>
+          <Text level="caption_2">Level 3</Text>
         </View>
-        <View style={styles.levelBarContainer}>
-          <View style={styles.levelBar} />
-        </View>
-        <Image
-          source={require("../../../assets/icon.png")} // Update this path if necessary
-          style={styles.image}
-        />
         <View style={styles.badgeContainer}>
-          <Text style={styles.badgeText}>Fire Weasel</Text>
+          <Image
+            source={require("../../../assets/icon.png")} // Update this path if necessary
+            style={styles.image}
+          />
+          <Text level="title_3">Fire Weasel</Text>
         </View>
       </View>
     </Modal>
@@ -36,52 +38,39 @@ const NewLevelModal: React.FC<NewLevelModalProps> = ({
 
 const styles = StyleSheet.create({
   modalContent: {
-    backgroundColor: "white",
-    padding: 20,
+    backgroundColor: globalStyles.colors.white,
+    paddingVertical: 50,
+    paddingHorizontal: 20,
     borderRadius: 10,
     alignItems: "center",
   },
   title: {
-    fontSize: 18,
-    fontWeight: "bold",
     marginBottom: 70,
-  },
-  image: {
-    width: 150,
-    height: 150,
-    marginVertical: 10,
   },
   levelContainer: {
     flexDirection: "row",
     justifyContent: "space-between",
     width: "100%",
     marginVertical: 10,
-  },
-  levelLabel: {
-    fontWeight: "bold",
+    marginBottom: 40,
   },
   levelBarContainer: {
     position: "absolute",
-    top: 110,
+    top: -20,
     width: "100%",
     height: 10,
-    backgroundColor: "#ccc",
-    marginHorizontal: 10,
+    backgroundColor: globalStyles.colors.secondary,
     borderRadius: 5,
     overflow: "hidden",
-  },
-  levelBar: {
-    width: "100%",
-    height: "100%",
-    backgroundColor: "gray",
   },
   badgeContainer: {
     marginVertical: 10,
     alignItems: "center",
   },
-  badgeText: {
-    fontSize: 16,
-    fontWeight: "bold",
+  image: {
+    width: 150,
+    height: 150,
+    marginVertical: 10,
   },
 });
 
