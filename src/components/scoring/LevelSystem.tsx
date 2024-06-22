bb0ac75eabdfa0dec4075ea0e466e6be8918526f
Reinhardt Botha <botha.wr@gmail.com>
2024-06-22 15:28:56 -0700

    feat: Level System

--- src/components/scoring/LevelSystem.tsx
+++ src/components/scoring/LevelSystem.tsx
@@ -1,3 +1,31 @@
-const LevelSystem = () => {};
+import levels from "@src/levels";
+import { useUser } from "@src/state/useUser";
+import { useEffect } from "react";
+
+const LevelSystem = () => {
+  const userXP = useUser((state) => state.user.xp);
+  const setLevel = useUser((state) => state.setLevel);
+
+  useEffect(() => {
+    const determineLevel = (xp: number) => {
+      let currentLevel = 1;
+      for (let i = 0; i < levels.length; i++) {
+        if (xp >= levels[i].xp) {
+          currentLevel = levels[i].level;
+        } else {
+          break;
+        }
+      }
+      return currentLevel;
+    };
+
+    const newLevel = determineLevel(userXP);
+    setLevel(newLevel);
+  }, [userXP, setLevel]);
+
+  return null;
+
+  //TODO: save the Level to the databse when new level reached
+};
 
 export default LevelSystem;
---
+++ src/levels.ts
@@ -0,0 +1,11 @@
+import { LevelType } from "./interfaces/level.types";
+
+const levels: LevelType[] = [
+  { level: 1, xp: 0 },
+  { level: 2, xp: 1000 },
+  { level: 3, xp: 3000 },
+  { level: 4, xp: 6000 },
+  { level: 5, xp: 10000 },
+];
+
+export default levels;
