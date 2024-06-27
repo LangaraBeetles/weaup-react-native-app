cfc8578d023c804f3928ab6faf36547225de0931
Victorportus <41453306+Victorportus@users.noreply.github.com>
2024-06-27 15:38:57 -0700

    feat: Svg handler (#33)
    * fix: fixed code as requested
    
    * feat: icon component
    
    * feat: SVG working with Icon component and nav menu layout working.
    
    * fix: ios files
    
    ---------
    
    Co-authored-by: Wonnnyo Hamester <wonnyohamester@outlook.com>

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,23 +1,75 @@
 import { Tabs } from "expo-router";
+import Icon from "@src/components/ui/Icon";
 
 const TabsLayout = () => {
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "#363430",
+        tabBarInactiveTintColor: "#363430",
+      }}
+    >
       <Tabs.Screen
         name="index"
-        options={{ headerShown: false, title: "Home" }}
+        options={{
+          headerShown: false,
+          title: "Home",
+          tabBarIcon: ({ focused }) => (
+            <Icon
+              name={
+                focused ? "navbar-home-selected" : "navbar-home-notselected"
+              }
+            />
+          ),
+        }}
       />
       <Tabs.Screen
         name="together"
-        options={{ headerTitle: "Ongoing Challenges", title: "Together" }}
+        options={{
+          headerTitle: "Ongoing Challenges",
+          title: "Together",
+          tabBarIcon: ({ focused }) => (
+            <Icon
+              name={
+                focused
+                  ? "navbar-together-selected"
+                  : "navbar-together-notselected"
+              }
+            />
+          ),
+        }}
       />
       <Tabs.Screen
         name="analytics"
-        options={{ headerTitle: "Analytics", title: "Analytics" }}
+        options={{
+          headerTitle: "Analytics",
+          title: "Analytics",
+          tabBarIcon: ({ focused }) => (
+            <Icon
+              name={
+                focused
+                  ? "navbar-analytics-selected"
+                  : "navbar-analytics-notselected"
+              }
+            />
+          ),
+        }}
       />
       <Tabs.Screen
         name="profile"
-        options={{ headerTitle: "Profile", title: "Profile" }}
+        options={{
+          headerTitle: "Profile",
+          title: "Profile",
+          tabBarIcon: ({ focused }) => (
+            <Icon
+              name={
+                focused
+                  ? "navbar-profile-selected"
+                  : "navbar-profile-notselected"
+              }
+            />
+          ),
+        }}
       />
     </Tabs>
   );
