896746e2c12e512d4351dadd13d4dcb5651f870d
Reinhardt Botha <botha.wr@gmail.com>
2024-07-01 11:25:36 -0700

    feat: add styleProps to Box with default values

--- src/components/ui/Box.tsx
+++ src/components/ui/Box.tsx
@@ -2,15 +2,25 @@ import { styled } from "@fast-styles/react";
 import { globalStyles } from "@src/styles/globalStyles";
 import { View } from "react-native";
 
-const Box = styled(View, {
+const StyledBox = styled(View, {
   display: "flex",
   width: "100%",
   padding: 16,
-  backgroundColor: globalStyles.colors.white,
   borderRadius: 16,
   borderStyle: "solid",
   borderWidth: 1,
-  borderColor: globalStyles.colors.neutral[100],
+  styleProps: {
+    bc: "borderColor",
+    bg: "backgroundColor",
+  },
 });
+
+const Box = ({
+  bc = globalStyles.colors.neutral[100],
+  bg = globalStyles.colors.white,
+  ...props
+}) => {
+  return <StyledBox bc={bc} bg={bg} {...props} />;
+};
 
 export default Box;
