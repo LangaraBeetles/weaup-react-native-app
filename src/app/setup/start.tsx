cbfd0d99d895ecc2407e21719a462b2607873f98
Victorportus <vic.portus@gmail.com>
2024-07-06 12:25:40 -0700

    work in progress

--- src/app/setup/start.tsx
+++ src/app/setup/start.tsx
@@ -1,10 +1,16 @@
-import Main from "@src/components/layout/Main";
 import Button from "@src/components/ui/Button";
 import Stack from "@src/components/ui/Stack";
 import { Text } from "@src/components/ui/typography";
 import { router } from "expo-router";
 import Checkbox from "expo-checkbox";
-import { SafeAreaView, View } from "react-native";
+import { Dimensions, SafeAreaView, StyleSheet, View } from "react-native";
+import Gradient from "@src/components/ui/Gradient";
+import { theme } from "@src/styles/theme";
+import Image from "@src/components/ui/Image";
+import Center from "@src/components/ui/Center";
+import Icon from "@src/components/ui/Icon";
+
+const { width, height } = Dimensions.get("window");
 
 const SetupPages = () => {
   const next = () => {
@@ -14,30 +20,41 @@ const SetupPages = () => {
   const login = () => {
     // router.push("provider-signup");
   };
-
   return (
-    <SafeAreaView>
-      <Main>
-        <View
-          style={{
-            justifyContent: "center",
-            height: "100%",
-            paddingHorizontal: 2,
-          }}
-        >
-          <View style={{ height: "40%" }} />
-          <Stack gap={80}>
-            <Stack gap={16}>
-              <Text align="center" level="title_1">
+    <SafeAreaView style={{ flex: 1, alignItems: "center" }}>
+      <Gradient
+        color1={theme.colors.primary[300]}
+        color2={theme.colors.white}
+        locations={[0, 0.7]}
+      />
+      <Center
+        w={236}
+        h={464}
+        style={{ marginHorizontal: "auto", marginTop: height * 0.2 }}
+      >
+        <Image name="weasel-happy" />
+      </Center>
+      <Stack>
+        <Icon name="colorLabelIcon-target" size={64} />
+      </Stack>
+      <View style={style.mainContainer}>
+        <Stack w={width} pt={40} px={width * 0.05}>
+          <Stack gap={height * 0.05}>
+            <Stack gap={1}>
+              <Text
+                align="center"
+                level="title_1"
+                style={{ color: theme.colors.primary[900] }}
+              >
                 Welcome to WeaUp
               </Text>
 
-              <Text style={{ textAlign: "center" }}>
+              <Text level="body" style={{ textAlign: "center" }}>
                 Let the cute weasel help you stay upright!
               </Text>
             </Stack>
 
-            <Stack>
+            <Stack gap={16}>
               <Button
                 title="Start the Journey"
                 onPress={next}
@@ -46,19 +63,36 @@ const SetupPages = () => {
 
               <Button title="Log in" onPress={login} variant="secondary" />
             </Stack>
-            <Stack flexDirection="row" gap={5}>
+            <Stack flexDirection="row" gap={12} px={6}>
               {/* TODO: Checkbox functionality */}
-              <Checkbox />
-              <Text>
-                I have read and agree to the Terms of Service and Privacy
-                Policy.
-              </Text>
+              <Stack pt={2}>
+                <Checkbox />
+              </Stack>
+              <Stack w={width * 0.75}>
+                <Text level="caption_1">
+                  I have read and agree to the Terms of Service and Privacy
+                  Policy.
+                </Text>
+              </Stack>
             </Stack>
           </Stack>
-        </View>
-      </Main>
+        </Stack>
+      </View>
     </SafeAreaView>
   );
 };
+
+const style = StyleSheet.create({
+  mainContainer: {
+    position: "absolute",
+    top: height * 0.5,
+    width: 865,
+    height: 865,
+    backgroundColor: theme.colors.white,
+    borderRadius: 865 / 2,
+    flexShrink: 0,
+    alignItems: "center",
+  },
+});
 
 export default SetupPages;
