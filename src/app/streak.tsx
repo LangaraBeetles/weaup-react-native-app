c06e411e3605bc02e30fd114bfc4918f4b3ea51d
Reinhardt Botha <botha.wr@gmail.com>
2024-07-07 18:13:01 -0700

    feat: initial StreakNotificationCard

--- src/app/streak.tsx
+++ src/app/streak.tsx
@@ -1,12 +1,20 @@
-import { Text } from "react-native";
+import StreakNotificationCard from "@src/components/streak/StreakNotificationCard";
+import Spacer from "@src/components/ui/Spacer";
+import { useUser } from "@src/state/useUser";
+// import { StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const StreakScreen = () => {
+  const userStreak = useUser((state) => state.user.dailyStreakCounter);
+
   return (
     <SafeAreaView>
-      <Text>Streak Page text</Text>
+      <Spacer height={160} />
+      <StreakNotificationCard streak={userStreak} />
     </SafeAreaView>
   );
 };
+
+// const styles = StyleSheet.create({});
 
 export default StreakScreen;
