86ea9566248c481879deee8e284a2a659b375e44
Reinhardt Botha <botha.wr@gmail.com>
2024-07-08 21:52:37 -0700

    feat: update user state and ProfileBadgeContainerPreview

--- src/interfaces/badges.types.ts
+++ src/interfaces/badges.types.ts
@@ -1,9 +1,17 @@
 import { BadgeName } from "@src/components/ui/Badge";
 
 export type BadgeType = {
+  id: number;
   title: string;
   subtitle: string;
+  description: string;
   badge: BadgeName;
+  color?: string;
   unlocked?: boolean;
   date?: string | null;
+};
+
+export type UserBadgeType = {
+  id: number;
+  date: string;
 };
