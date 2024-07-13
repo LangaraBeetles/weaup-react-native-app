aef6f4d1474134e68c136419cae85f3c68f69944
Wonnnyo Hamester <wonnyohamester@outlook.com>
2024-07-13 13:32:08 -0700

    feat: google button

--- src/app/setup/signUp.tsx
+++ src/app/setup/signUp.tsx
@@ -113,37 +113,37 @@ const SignUp = () => {
               title="Continue with Google"
               leadingIcon="google-icon"
             />
-            {/* <GoogleSigninButton
+            <GoogleSigninButton
               size={GoogleSigninButton.Size.Wide}
               onPress={async () => {
-                try {
-                  await GoogleSignin.hasPlayServices();
-                  const userInfo = await GoogleSignin.signIn();
-                  // console.log({ userInfo });
-                  // setState({ userInfo, error: undefined });
-                } catch (error) {
-                  console.log({ error });
-                  if (isErrorWithCode(error)) {
-                    switch (error.code) {
-                      case statusCodes.SIGN_IN_CANCELLED:
-                        // user cancelled the login flow
-                        break;
-                      case statusCodes.IN_PROGRESS:
-                        // operation (eg. sign in) already in progress
-                        break;
-                      case statusCodes.PLAY_SERVICES_NOT_AVAILABLE:
-                        // play services not available or outdated
-                        break;
-                      default:
-                      // some other error happened
-                    }
-                  } else {
-                    // an error that's not related to google sign in occurred
-                  }
-                }
+                // try {
+                //   await GoogleSignin.hasPlayServices();
+                //   const userInfo = await GoogleSignin.signIn();
+                //   // console.log({ userInfo });
+                //   // setState({ userInfo, error: undefined });
+                // } catch (error) {
+                //   console.log({ error });
+                //   if (isErrorWithCode(error)) {
+                //     switch (error.code) {
+                //       case statusCodes.SIGN_IN_CANCELLED:
+                //         // user cancelled the login flow
+                //         break;
+                //       case statusCodes.IN_PROGRESS:
+                //         // operation (eg. sign in) already in progress
+                //         break;
+                //       case statusCodes.PLAY_SERVICES_NOT_AVAILABLE:
+                //         // play services not available or outdated
+                //         break;
+                //       default:
+                //       // some other error happened
+                //     }
+                //   } else {
+                //     // an error that's not related to google sign in occurred
+                //   }
+                // }
               }}
               disabled={false}
-            /> */}
+            />
 
             <TouchableOpacity onPress={handleContinueAsGuest}>
               <Text
