a4b4e5f38dfd376987a5983234282f8c623f5a67
Wonnnyo Hamester <wonnyohamester@outlook.com>
2024-07-12 19:03:17 -0700

    feat: profile changes

--- src/components/container/ProfileBadgeContainerPreview.tsx
+++ src/components/container/ProfileBadgeContainerPreview.tsx
@@ -2,61 +2,71 @@ import { useUser } from "@src/state/useUser";
 import Stack from "../ui/Stack";
 import BadgeContainer from "./BadgeContainer";
 import badges from "@src/badges";
+import { UserBadgeType } from "@src/interfaces/badges.types";
+
+const getLatestBadges = (userBadges: Array<UserBadgeType>) => {
+  try {
+   return userBadges?.length
+     ? userBadges
+         ?.map((userBadge) => {
+           const badge = badges.find((b) => b.id === userBadge.id);
+           if (badge) {
+             return { ...badge, date: userBadge.date };
+           }
+           return null;
+         })
+         ?.filter((badge) => !!badge?.date)
+         ?.sort(
+           (a, b) =>
+             new Date(b?.date as string).getTime() -
+             new Date(a?.date as string).getTime()
+         )
+         ?.slice(0, 3)
+     : [];
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+};
 
 const ProfileBadgeContainerPreview = () => {
   const userBadges = useUser((state) => state.user.badges);
 
-  const latestBadges = userBadges
-    ? userBadges
-        ?.map((userBadge) => {
-          const badge = badges.find((b) => b.id === userBadge.id);
-          if (badge) {
-            return { ...badge, date: userBadge.date };
-          }
-          return null;
-        })
-        ?.filter((badge) => !!badge?.date)
-        ?.sort(
-          (a, b) =>
-            new Date(b?.date as string).getTime() -
-            new Date(a?.date as string).getTime()
-        )
-        ?.slice(0, 3)
-    : [];
+  const latestBadges = getLatestBadges(userBadges);
 
-  const renderBadges = () => {
-    return latestBadges?.map((badge, index) =>
-      badge ? (
-        <BadgeContainer
-          key={index}
-          id={badge.id}
-          title={badge.title}
-          subtitle={badge.subtitle}
-          description={badge.description}
-          badge={badge.badge}
-          color={badge.color}
-          unlocked={true}
-        />
-      ) : null
-    );
-  };
+  // const renderBadges = () => {
+  //   return latestBadges?.map((badge, index) =>
+  //     badge ? (
+  //       <BadgeContainer
+  //         key={index}
+  //         id={badge.id}
+  //         title={badge.title}
+  //         subtitle={badge.subtitle}
+  //         description={badge.description}
+  //         badge={badge.badge}
+  //         color={badge.color}
+  //         unlocked={true}
+  //       />
+  //     ) : null
+  //   );
+  // };
 
-  const renderLockedBadges = () => {
-    return badges
-      .slice(0, 3)
-      .map((badge, index) => (
-        <BadgeContainer
-          key={index}
-          id={badge.id}
-          title={badge.title}
-          subtitle={badge.subtitle}
-          description={badge.description}
-          badge={badge.badge}
-          color={badge.color}
-          unlocked={false}
-        />
-      ));
-  };
+  // const renderLockedBadges = () => {
+  //   return badges
+  //     .slice(0, 3)
+  //     .map((badge, index) => (
+  //       <BadgeContainer
+  //         key={index}
+  //         id={badge.id}
+  //         title={badge.title}
+  //         subtitle={badge.subtitle}
+  //         description={badge.description}
+  //         badge={badge.badge}
+  //         color={badge.color}
+  //         unlocked={false}
+  //       />
+  //     ));
+  // };
 
   return (
     <Stack
@@ -64,7 +74,7 @@ const ProfileBadgeContainerPreview = () => {
       gap={16}
       justifyContent={latestBadges.length < 3 ? "flex-start" : "space-between"}
     >
-      {latestBadges.length > 0 ? renderBadges() : renderLockedBadges()}
+      {/* {latestBadges.length > 0 ? renderBadges() : renderLockedBadges()} */}
     </Stack>
   );
 };
