cb2c8756a42ede08a826816f2352977346e1d263
Victorportus <vic.portus@gmail.com>
2024-07-18 01:29:41 -0700

    fix: config

--- src/config.ts
+++ src/config.ts
@@ -5,8 +5,7 @@ const config = {
     secretAccessKey: "",
     region: "",
   },
-  api_url: "http://localhost:3000/api/v1/",
-  // api_url: "https://dev.api.weaup.io/api/v1/",
+  api_url: "https://dev.api.weaup.io/api/v1/",
 };
 
 export default config;
