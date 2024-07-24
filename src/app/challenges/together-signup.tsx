03ae355a3f5238f6778daba84203f2720dd78efe
Victorportus <vic.portus@gmail.com>
2024-07-24 14:32:06 -0700

    feat: Together Signup refactor

--- src/app/challenges/together-signup.tsx
+++ src/app/challenges/together-signup.tsx
@@ -1,103 +1,107 @@
-import {
-  Dimensions,
-  Platform,
-  SafeAreaView,
-  StyleSheet,
-  View,
-} from "react-native";
+import { Dimensions, SafeAreaView, StyleSheet, View } from "react-native";
 
 import Stack from "@src/components/ui/Stack";
 import { Text } from "@src/components/ui/typography";
 import { theme } from "@src/styles/theme";
 import Image from "@src/components/ui/Image";
 import Center from "@src/components/ui/Center";
-import Icon from "@src/components/ui/Icon";
+import Icon, { IconName } from "@src/components/ui/Icon";
 import GoogleButton from "@root/src/components/ui/GoogleButton";
 import { LinearGradient } from "expo-linear-gradient";
 
 const { width, height } = Dimensions.get("window");
 
+const ListComponent = ({
+  text,
+  iconName,
+}: {
+  text: string;
+  iconName: IconName;
+}) => {
+  return (
+    <Stack flexDirection="row" gap={16}>
+      <Stack
+        backgroundColor={theme.colors.secondary[500]}
+        p={6}
+        borderRadius={8}
+        h={36}
+        w={36}
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Icon name={iconName} />
+      </Stack>
+      <Stack flex={1} justifyContent="center">
+        <Text level="callout">{text}</Text>
+      </Stack>
+    </Stack>
+  );
+};
+
 const TogetherSignUp = () => {
   return (
-    <SafeAreaView style={styles.main}>
-      <Stack
-        py={16}
-        px={10}
-        pt={Platform.OS === "android" ? height * 0.08 : 16}
-        flex={1}
-      >
-        <Stack flexGrow={1} gap={20}>
-          <Center>
-            <Text style={styles.content} level="title_2">
-              How Together Works
-            </Text>
-          </Center>
+    <SafeAreaView>
+      <Stack gap={20} h={"100%"}>
+        <Center pt={20}>
+          <Text level="title_2">How Together Works</Text>
+        </Center>
 
-          <View style={styles.mainContainer}>
-            <LinearGradient
-              colors={[theme.colors.primary[200], theme.colors.white]}
-              locations={[0.2, 0.4]}
-              style={{
-                position: "absolute",
-                zIndex: -1,
-                width: width * 2,
-                height: height * 2,
-                borderRadius: width,
-              }}
-            />
-          </View>
+        <View style={styles.mainContainer}>
+          <LinearGradient
+            colors={[theme.colors.primary[400], theme.colors.white]}
+            locations={[0.1, 0.4]}
+            style={{
+              position: "absolute",
+              zIndex: -1,
+              width: width * 2,
+              height: height * 2,
+              borderRadius: width,
+            }}
+          />
+        </View>
+        <Center h={348} style={{ zIndex: -2, top: 10 }}>
+          <Image name="weasel-happy" />
+        </Center>
 
-          <Stack
-            flexGrow={0}
-            alignItems="center"
-            style={styles.paddedContent}
-            justifyContent="space-between"
-            gap={20}
-            h={height * 0.7}
-            pb={0}
-          >
-            <Stack w="50%" h={150} gap={15} alignItems="center">
-              <Image name="weasel-floating" />
-              <Image name="elipse-shadow" height={"10%"} width={"70%"} />
-            </Stack>
-
+        <Stack
+          gap={20}
+          h={409}
+          px={20}
+          w={"100%"}
+          style={{ position: "absolute", bottom: 20 }}
+        >
+          <Stack>
             <Text level="title_3" align="center">
               Improve your posture with your friends using WeaUp!
             </Text>
-
-            <Stack
-              py={28}
-              px={16}
-              gap={20}
-              backgroundColor={theme.colors.white}
-              borderRadius={16}
-            >
-              <Stack flexDirection="row" gap={16}>
-                <View style={styles.starList}>
-                  <Icon name="star-outline" color={theme.colors.white} />
-                </View>
-                <Text level="callout">
-                  Get Support and Stay Motivated Together
-                </Text>
-              </Stack>
-              <Stack flexDirection="row" gap={16}>
-                <View style={styles.starList}>
-                  <Icon name="star-outline" color={theme.colors.white} />
-                </View>
-                <Text level="callout">Join Exclusive Group Challenges</Text>
-              </Stack>
-              <Stack flexDirection="row" gap={16}>
-                <View style={styles.starList}>
-                  <Icon name="star-outline" color={theme.colors.white} />
-                </View>
-                <Text level="callout">Earn Group Rewards</Text>
-              </Stack>
-            </Stack>
-
-            <Text level="headline">Unlimited free access!</Text>
           </Stack>
 
-          <GoogleButton signUp={true} />
+          <Stack
+            py={28}
+            px={16}
+            gap={24}
+            backgroundColor={theme.colors.white}
+            borderRadius={16}
+          >
+            <ListComponent
+              text="Get Support and Stay Motivated Together"
+              iconName="together-icon-1"
+            />
+            <ListComponent
+              text="Join Exclusive Group Challenges"
+              iconName="together-icon-2"
+            />
+            <ListComponent
+              text="Earn Group Rewards"
+              iconName="together-icon-3"
+            />
+          </Stack>
+          <Stack alignItems="center">
+            <Text level="headline">Unlimited free access!</Text>
+          </Stack>
+          <Stack>
+            <GoogleButton signUp={true} />
+          </Stack>
         </Stack>
       </Stack>
     </SafeAreaView>
@@ -107,29 +111,13 @@ const TogetherSignUp = () => {
 const styles = StyleSheet.create({
   mainContainer: {
     position: "absolute",
-    top: height * 0.15,
+    top: height * 0.25,
     width: width * 2,
     height: height * 2,
     backgroundColor: theme.colors.primary[200],
     borderRadius: width,
     flexShrink: 0,
     alignSelf: "center",
-  },
-  main: {
-    // height: "50%",
-  },
-  content: {
-    flexGrow: 2,
-    color: theme.colors.primary[900],
-  },
-  paddedContent: {
-    paddingHorizontal: 26,
-  },
-  starList: {
-    backgroundColor: theme.colors.secondary[500],
-    borderRadius: 12,
-    alignSelf: "center",
-    padding: 6,
   },
 });
 
