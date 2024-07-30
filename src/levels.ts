c20eb1b48078440dba33771472963980f398bacb
Reinhardt Botha <botha.wr@gmail.com>
2024-07-29 19:36:27 -0700

    feat: update levels.ts

--- src/levels.ts
+++ src/levels.ts
@@ -2,16 +2,16 @@ import { LevelType } from "./interfaces/level.types";
 
 //TODO: adjust level values
 const levels: LevelType[] = [
-  // { level: 1, xp: 0 },
-  // { level: 2, xp: 20 },
-  // { level: 3, xp: 40 },
-  // { level: 4, xp: 50 },
-  // { level: 5, xp: 180 },
-  // { level: 6, xp: 370 },
-  ...new Array(100).fill({}).map((_, index) => {
-    const level = index + 1;
-    return { level: level, xp: level * 100 };
-  }),
+  { level: 1, xp: 0 },
+  { level: 2, xp: 20 },
+  { level: 3, xp: 1000 },
+  { level: 4, xp: 2000 },
+  { level: 5, xp: 3000 },
+  { level: 6, xp: 4000 },
+  // ...new Array(100).fill({}).map((_, index) => {
+  //   const level = index + 1;
+  //   return { level: level, xp: level * 100 };
+  // }),
 ];
 
 export default levels;
