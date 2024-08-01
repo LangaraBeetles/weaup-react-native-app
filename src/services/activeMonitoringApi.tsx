b42f8edcbcb5ca8c9ff82b6ae808a48302dfec11
Wonnyo Hamester <57273227+whamester@users.noreply.github.com>
2024-08-01 13:55:06 -0700

    fix: total duration (#135)
    * feat: remove lorem ipsum
    
    * fix: earbuds connected screen displayed only after the earbuds-mode-confirmation page
    
    * fix: analytics page - total duration
    
    * fix: total duration

--- src/services/activeMonitoringApi.tsx
+++ src/services/activeMonitoringApi.tsx
@@ -1,11 +1,21 @@
 import api from "./api";
+import dayjs from "dayjs";
 import { activeMonitoringType } from "@src/interfaces/activemonitoring.types";
 
 const route = "active_monitoring";
 
-const getActiveMonitoring = async () => {
-  const { data } = await api.get(route);
-  return data;
+const getActiveMonitoring = async (start?: string, end?: string) => {
+  const start_date = dayjs(start ?? "").format("YYYY-MM-DD");
+  const end_date = dayjs(end ?? "").format("YYYY-MM-DD");
+
+  const { data } = await api.get(route, {
+    params: {
+      start_date,
+      end_date,
+    },
+  });
+
+  return data.data;
 };
 
 const saveActiveMonitoring = async (activeMonitoring: activeMonitoringType) => {
