70c473169304e647801053c8fee2a5f1d5564d69
zeckkimon <bsaguid@gmail.com>
2024-08-11 04:59:49 -0700

    fix select mode animation

--- src/components/setup/SelectMode.tsx
+++ src/components/setup/SelectMode.tsx
@@ -4,6 +4,7 @@ import Animated, {
   runOnJS,
   useAnimatedStyle,
   useSharedValue,
+  withRepeat,
   withSpring,
   withTiming,
 } from "react-native-reanimated";
@@ -17,6 +18,7 @@ import Image from "@src/components/ui/Image";
 import { useUser } from "@root/src/state/useUser";
 import { TrackingModeEnum } from "@root/src/interfaces/user.types";
 import Icon from "@root/src/components/ui/Icon";
+import { useEffect } from "react";
 
 const { height: screenHeight } = Dimensions.get("window");
 const centerY = screenHeight / 2;
@@ -34,6 +36,7 @@ const SelectMode: React.FC<SelectModeProps> = ({ changePage }) => {
   const earbudsWeaselY = useSharedValue(0);
   const earbudsYellowScale = useSharedValue(1);
   const earbudsYellowOpacity = useSharedValue(1);
+  const yellowCardY = useSharedValue(0);
   const earbudsTextOpacity = useSharedValue(1);
 
   const phoneContainerOpacity = useSharedValue(1);
@@ -42,26 +45,63 @@ const SelectMode: React.FC<SelectModeProps> = ({ changePage }) => {
   const phoneYellowScale = useSharedValue(1);
   const phoneYellowOpacity = useSharedValue(1);
   const phoneTextOpacity = useSharedValue(1);
+  const chevronOpacity = useSharedValue(0);
+  const duration = 1000;
 
   const AnimatedImage = Animated.createAnimatedComponent(Image);
+  useEffect(() => {
+    chevronOpacity.value = withRepeat(withTiming(1, { duration }), -1, true);
+  }, []);
+
   const clamp = (value: number, min: number, max: number) => {
     "worklet";
+    return Math.min(Math.max(value, min), max);
+  };
+
+  const clampEarbudsWeasel = (value: number, min: number, max: number) => {
+    "worklet";
+
+    if (value > max / 2) {
+      if (max - value <= max * -1) {
+        return max * -1;
+      }
+      return max - value;
+    }
+    return Math.min(Math.max(value, min), max);
+  };
+
+  const clampPhoneWeasel = (value: number, min: number, max: number) => {
+    "worklet";
+
+    if (min / 2 > value) {
+      if (min - value >= min * -1) {
+        return min * -1;
+      }
+      return min - value;
+    }
     return Math.min(Math.max(value, min), max);
   };
 
   //gesture for earbuds mode
   const onEarbudsDrag = Gesture.Pan()
     .onChange((event) => {
+      earbudsWeaselY.value = clampEarbudsWeasel(
+        event.translationY,
+        0,
+        centerY / 2,
+      );
+      yellowCardY.value = event.translationY * 1.5;
       earbudsContainerY.value = clamp(event.translationY, 0, centerY);
       phoneContainerOpacity.value = withTiming(0);
     })
     .onFinalize(() => {
       if (earbudsContainerY.value < centerY) {
-        earbudsContainerY.value = withSpring(0, { stiffness: 50 });
-        earbudsWeaselY.value = withSpring(0, { stiffness: 50 });
-        earbudsYellowScale.value = withSpring(1, { stiffness: 100 });
+        earbudsContainerY.value = withTiming(0);
+        earbudsWeaselY.value = withTiming(0);
+        yellowCardY.value = withTiming(0);
+        earbudsYellowScale.value = withTiming(0);
         earbudsYellowOpacity.value = withTiming(1);
-        earbudsTextOpacity.value = withTiming(1);
+        earbudsTextOpacity.value = withTiming(1); // withSpring(1,  { stiffness: 100 });
         phoneContainerOpacity.value = withTiming(1, { duration: 1000 });
       } else {
         earbudsYellowScale.value = withTiming(10, { duration: 500 });
@@ -85,6 +125,11 @@ const SelectMode: React.FC<SelectModeProps> = ({ changePage }) => {
   //gesture for phone mode
   const onPhoneDrag = Gesture.Pan()
     .onChange((event) => {
+      phoneWeaselY.value = clampPhoneWeasel(
+        event.translationY,
+        -centerY / 2,
+        0,
+      );
       phoneContainerY.value = clamp(event.translationY, -centerY, 0);
       earbudsContainerOpacity.value = withTiming(0);
     })
@@ -126,6 +171,17 @@ const SelectMode: React.FC<SelectModeProps> = ({ changePage }) => {
     };
   });
 
+  const yellowCardStyle = useAnimatedStyle(() => {
+    return {
+      opacity: earbudsContainerOpacity.value,
+      transform: [
+        {
+          translateY: withTiming(yellowCardY.value * 1.5, { duration: 50 }),
+        },
+      ],
+    };
+  });
+
   const earbudsWeaselStyle = useAnimatedStyle(() => {
     return {
       transform: [
@@ -138,10 +194,10 @@ const SelectMode: React.FC<SelectModeProps> = ({ changePage }) => {
 
   const earbudsYellowStyle = useAnimatedStyle(() => {
     return {
-      opacity: earbudsYellowOpacity.value,
+      opacity: interpolate(earbudsContainerY.value, [100, 0], [1.5, 2]),
       transform: [
         {
-          scale: interpolate(earbudsContainerY.value, [0, 100], [1, 2]),
+          scale: interpolate(earbudsContainerY.value, [0, 50], [1, 2]),
         },
       ],
     };
@@ -152,8 +208,13 @@ const SelectMode: React.FC<SelectModeProps> = ({ changePage }) => {
       opacity: interpolate(
         earbudsContainerY.value,
         [100, 0], // Input range (e.g., yValue range from 0 to 100)
-        [1, 2], // Output range (scale from 1 to 2)
+        [0, 1], // Output range (scale from 1 to 2)
       ),
+      transform: [
+        {
+          translateY: withSpring(earbudsWeaselY.value),
+        },
+      ],
     };
   });
 
@@ -180,10 +241,10 @@ const SelectMode: React.FC<SelectModeProps> = ({ changePage }) => {
 
   const phoneYellowStyle = useAnimatedStyle(() => {
     return {
-      opacity: phoneYellowOpacity.value,
+      opacity: interpolate(phoneContainerY.value, [-200, 0], [1.5, 2]),
       transform: [
         {
-          scale: interpolate(phoneContainerY.value, [-100, 100], [0, 2]),
+          scale: interpolate(phoneContainerY.value, [-50, 0], [2, 1]),
         },
       ],
     };
@@ -193,9 +254,20 @@ const SelectMode: React.FC<SelectModeProps> = ({ changePage }) => {
     return {
       opacity: interpolate(
         phoneContainerY.value,
-        [-100, 100], // Input range (e.g., yValue range from 0 to 100)
+        [-50, 0], // Input range (e.g., yValue range from 0 to 100)
         [1, 2], // Output range (scale from 1 to 2)
       ),
+      transform: [
+        {
+          translateY: withSpring(phoneWeaselY.value),
+        },
+      ],
+    };
+  });
+
+  const chevronStyle = useAnimatedStyle(() => {
+    return {
+      opacity: chevronOpacity.value,
     };
   });
 
@@ -228,6 +300,18 @@ const SelectMode: React.FC<SelectModeProps> = ({ changePage }) => {
                   borderRadius: 20,
                 }}
               >
+                <Animated.View
+                  style={[
+                    yellowCardStyle,
+                    {
+                      width: "100%",
+                      height: screenHeight * 2,
+                      backgroundColor: theme.colors.primary[50],
+                      alignItems: "center",
+                      borderRadius: 20,
+                    },
+                  ]}
+                ></Animated.View>
                 <Center
                   style={{
                     width: "70%",
@@ -282,7 +366,12 @@ const SelectMode: React.FC<SelectModeProps> = ({ changePage }) => {
                         Use earbuds (AirPods 3 / Pro / Max) to track posture
                       </Text>
                     </Stack>
-                    <Icon name="double-chevron-down" style={{ bottom: "3%" }} />
+                    <Animated.View style={[chevronStyle]}>
+                      <Icon
+                        name="double-chevron-down"
+                        style={{ bottom: "3%" }}
+                      />
+                    </Animated.View>
                   </Animated.View>
                 </Center>
               </View>
@@ -328,7 +417,9 @@ const SelectMode: React.FC<SelectModeProps> = ({ changePage }) => {
                     top: "3%",
                   }}
                 >
-                  <Icon name="double-chevron-up" style={{ top: "3%" }} />
+                  <Animated.View style={[chevronStyle]}>
+                    <Icon name="double-chevron-up" style={{ top: "32%" }} />
+                  </Animated.View>
 
                   <AnimatedImage
                     name="yellow-circle"
